fde1772da4ccf32d2eb59c3387ac97168901840e
Developer-For-Git <developerforgit2@gmail.com>
2026-09-25 23:00:42 -0700

    Release DisciplineOS v2.20.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.discipline.os"
         minSdk = 26
         targetSdk = 35
-        versionCode = 21
-        versionName = "2.19.0"
+        versionCode = 22
+        versionName = "2.20.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
